459b54bfeb4b113043718935e2bb2fa10082fc1a
Andrea Turri <Andrea@macbookdiandrea.lan>
2014-12-14 17:23:28 +0100

    User entity

--- MeteoCal/src/main/java/it/polimi/meteocal/business/entity/User.java
+++ MeteoCal/src/main/java/it/polimi/meteocal/business/entity/User.java
@@ -6,53 +6,193 @@
 package it.polimi.meteocal.business.entity;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 /**
  *
  * @author Fabiuz
  */
-@Entity
+@Entity(name = "user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @Pattern(regexp = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+            message = "Not valid email")
+    @NotNull(message = "May not be empty")
+    private String email;
+    
+    @NotNull(message = "May not be empty")
+    private String password;
+    
+    @NotNull(message = "May not be empty")
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date birthdate;
+    
+    @NotNull(message = "May not be empty")
+    private String name;
+    
+    @NotNull(message = "May not be empty")
+    private String surname;
+    
+    @NotNull(message = "May not be empty")
+    private char gender;
 
-    public Long getId() {
-        return id;
+    @NotNull(message = "May not be empty")
+    private String address;
+      
+    @NotNull(message = "May not be empty")
+    private String city;
+    
+    private String photo;
+ 
+    @NotNull(message = "May not be empty")
+    private Boolean privateCalendar;
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
     }
 
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof User)) {
-            return false;
-        }
-        User other = (User) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
     }
 
-    @Override
-    public String toString() {
-        return "it.polimi.meteocal.business.entity.user[ id=" + id + " ]";
+    /**
+     * @return the birthdate
+     */
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    /**
+     * @param birthdate the birthdate to set
+     */
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the surname
+     */
+    public String getSurname() {
+        return surname;
+    }
+
+    /**
+     * @param surname the surname to set
+     */
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    /**
+     * @return the gender
+     */
+    public char getGender() {
+        return gender;
+    }
+
+    /**
+     * @param gender the gender to set
+     */
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the city
+     */
+    public String getCity() {
+        return city;
+    }
+
+    /**
+     * @param city the city to set
+     */
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    /**
+     * @return the photo
+     */
+    public String getPhoto() {
+        return photo;
+    }
+
+    /**
+     * @param photo the photo to set
+     */
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    /**
+     * @return the privateCalendar
+     */
+    public Boolean getPrivateCalendar() {
+        return privateCalendar;
+    }
+
+    /**
+     * @param privateCalendar the privateCalendar to set
+     */
+    public void setPrivateCalendar(Boolean privateCalendar) {
+        this.privateCalendar = privateCalendar;
     }
     
+
 }
