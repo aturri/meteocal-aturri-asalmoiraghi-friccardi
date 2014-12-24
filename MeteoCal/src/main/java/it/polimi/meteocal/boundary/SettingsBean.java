3172249b3543b168c7e7b010176196607b13ddda
Fabiuz <Fabiuz@192.168.1.3>
2014-12-24 12:56:29 +0100

    Create the settings.xhtml page that allow to change the user's setting. It works, but the GUI and the error messages must be set up a little fine.

---
+++ MeteoCal/src/main/java/it/polimi/meteocal/boundary/SettingsBean.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.meteocal.boundary;
+
+import it.polimi.meteocal.control.Utility;
+import it.polimi.meteocal.entity.User;
+import it.polimi.meteocal.entityManager.UserManager;
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+/**
+ *
+ * @author Fabiuz
+ */
+@Named
+@RequestScoped
+public class SettingsBean {
+
+    @EJB
+    UserManager userManager;
+    
+    private User user;
+    
+    private String oldPassword;
+    private String newPassword1;
+    private String newPassword2;
+    
+    public SettingsBean() {
+    }
+
+    public String changePassword(){
+        if(!this.newPassword1.equals(this.newPassword2)){
+            MessageBean.addError("Passwords don't match");
+        }else{
+            //verify the old password is correct
+            String old=Utility.getHashSHA256(this.oldPassword);
+            User currentUser=userManager.getLoggedUser();
+            String saved=currentUser.getPassword();
+            if(!saved.equals(old)){
+                MessageBean.addError("Insert the right password before change it");
+            }else{
+                //password update
+                //NB: we don't need to hash the new password. This is computed from glassfish automatically
+                currentUser.setPassword(this.newPassword1);
+                userManager.update(currentUser);
+                MessageBean.addInfo("The password is correctly changed");
+            }
+        }
+        return "";
+    }
+    
+    public String changeUsersData(){
+        userManager.update(this.user);
+        MessageBean.addInfo("Data correctly changed");
+        return "";
+    }
+    
+    public String changeCalendarVisibilityData(){
+        userManager.update(this.user);
+        MessageBean.addInfo("Your calendar's visibility is correctly changed");
+        return "";
+    }
+    /**
+     * @return the oldPassword
+     */
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    /**
+     * @param oldPassword the oldPassword to set
+     */
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    /**
+     * @return the newPassword1
+     */
+    public String getNewPassword1() {
+        return newPassword1;
+    }
+
+    /**
+     * @param newPassword1 the newPassword1 to set
+     */
+    public void setNewPassword1(String newPassword1) {
+        this.newPassword1 = newPassword1;
+    }
+
+    /**
+     * @return the newPassword2
+     */
+    public String getNewPassword2() {
+        return newPassword2;
+    }
+
+    /**
+     * @param newPassword2 the newPassword2 to set
+     */
+    public void setNewPassword2(String newPassword2) {
+        this.newPassword2 = newPassword2;
+    }
+
+    /**
+     * @return the user
+     */
+    public User getUser() {
+        if(this.user==null){
+            this.user=userManager.getLoggedUser();
+        }
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    
+    
+}
---
+++ MeteoCal/src/main/java/it/polimi/meteocal/control/Utility.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.meteocal.control;
+
+import it.polimi.meteocal.boundary.RecoverPasswordBean;
+import it.polimi.meteocal.entity.User;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *  This class contains some utility like hashing function
+ * @author Fabiuz
+ */
+public class Utility {
+     /**
+     * This method provide the hashed string of the input string (with SHA-256 algorithm)
+     * @param string the string on which will be executed the hashing function
+     * @return the string that represents the code
+     */
+    public static String getHashSHA256(String string){        
+        MessageDigest md=null;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(RecoverPasswordBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         
+        md.update(string.getBytes());
+        
+        byte[] mdbytes = md.digest();
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < mdbytes.length; i++) {
+          sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+        System.out.println("Hex format : " + sb.toString());
+
+        return sb.toString();
+    }
+}
