05e50d527bb4477bffa6c01f7a1b0fb95da6dc95
Fabiuz <Fabiuz@192.168.1.3>
2014-12-27 21:40:27 +0100

    Merged of registrationBean, nothing of relevant

--- MeteoCal/src/main/java/it/polimi/meteocal/boundary/RegistrationBean.java
+++ MeteoCal/src/main/java/it/polimi/meteocal/boundary/RegistrationBean.java
@@ -5,6 +5,7 @@
  */
 package it.polimi.meteocal.boundary;
 
+import it.polimi.meteocal.control.KindOfEmail;
 import it.polimi.meteocal.control.MailControler;
 import it.polimi.meteocal.control.NavigationBean;
 import it.polimi.meteocal.entityManager.UserManager;
@@ -31,7 +32,6 @@ public class RegistrationBean {
     
     @Resource(name = "mail/mailSession")
     private Session mailSession;
-    private MailControler mailControl;
     
     @EJB
     private UserManager um;
@@ -58,9 +58,8 @@ public class RegistrationBean {
     public String register() {
         try {
             um.save(user);
-            mailControl=new MailControler(mailSession);
-            mailControl.sendMail(user.getEmail(),user.getName()+" "+user.getSurname(),"Confirm registration",
-                    "Congraturation "+user.getName()+" "+user.getSurname()+",<br />Your account is registred succefully!<br />Best regards,<br />       MeteoCal's Team");
+            MailControler mailControler=new MailControler(this.mailSession);
+            mailControler.sendMail(user.getEmail(),KindOfEmail.REGISTRATION,null);
             return NavigationBean.redirectToLogin();
         }catch(MessagingException ex) {
             Logger.getLogger(RegistrationBean.class.getName()).log(Level.SEVERE, null, ex);
