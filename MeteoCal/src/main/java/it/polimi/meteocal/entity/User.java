3adeb3891f6e90e74d6271599300a2c9fc7ca7ae
Fabiuz <Fabiuz@192.168.1.3>
2014-12-27 21:29:56 +0100

    Refactor della classe user

--- MeteoCal/src/main/java/it/polimi/meteocal/entity/User.java
+++ MeteoCal/src/main/java/it/polimi/meteocal/entity/User.java
@@ -5,16 +5,11 @@
  */
 package it.polimi.meteocal.entity;
 
+import it.polimi.meteocal.control.Utility;
 import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 import java.util.Objects;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -97,14 +92,7 @@ public class User implements Serializable {
     }
 
     public void setPassword(String password) {
-        try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(password.getBytes("UTF-8"));
-            BigInteger bigInt = new BigInteger(1, hash);
-            this.password = bigInt.toString(16);
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException ex) {
-            Logger.getLogger(User.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        this.password = Utility.getHashSHA256(password);
     }
 
     public void setGroupName(String groupName) {
