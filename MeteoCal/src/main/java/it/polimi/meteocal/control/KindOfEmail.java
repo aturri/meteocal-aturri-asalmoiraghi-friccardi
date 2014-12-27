7d6ecb3d2b2f2ee2917bd9dc832f37f7e188f531
Fabiuz <Fabiuz@192.168.1.3>
2014-12-27 21:52:38 +0100

    Aggiunta della enumerazione per far funzionare tutto il resto delle email

---
+++ MeteoCal/src/main/java/it/polimi/meteocal/control/KindOfEmail.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.polimi.meteocal.control;
+
+/**
+ *
+ * @author Fabiuz
+ */
+public enum KindOfEmail {
+    REGISTRATION,INVITEDTOEVENT,FORGOTTENPASSWORD
+}
