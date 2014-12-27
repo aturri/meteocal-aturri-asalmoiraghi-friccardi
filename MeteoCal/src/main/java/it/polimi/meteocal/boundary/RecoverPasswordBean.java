7d2f5e427e794d6976ccc093f666295b9c65f039
Fabiuz <Fabiuz@192.168.1.3>
2014-12-27 21:46:06 +0100

    Refactor of this file

--- MeteoCal/src/main/java/it/polimi/meteocal/boundary/RecoverPasswordBean.java
+++ MeteoCal/src/main/java/it/polimi/meteocal/boundary/RecoverPasswordBean.java
@@ -7,11 +7,11 @@ package it.polimi.meteocal.boundary;
 
 //import com.google.common.base.Charsets;
 //import com.google.common.hash.Hashing;
+import it.polimi.meteocal.control.KindOfEmail;
 import it.polimi.meteocal.control.MailControler;
 import it.polimi.meteocal.control.NavigationBean;
 import it.polimi.meteocal.entity.User;
 import it.polimi.meteocal.entityManager.UserManager;
-import java.io.FileInputStream;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -60,16 +60,8 @@ public class RecoverPasswordBean {
             User user=userManager.findByEmail(email);
             mailControl=new MailControler(mailSession);
             try {
-                this.mailControl.sendMail(email, user.getName()+" "+user.getSurname(),"Recover your Meteocal's password", 
-                        "Dear "+ user.getName()+" "+user.getSurname()+",<br />"
-                                + "We have see your request to change your password because you have forgotten it. <br />"
-                                + "Now you just click on the follow link to set a new password and come back to MeteoCal<br /><br />"
-                                + "<a href=\""+this.getLinkForResetEmail(user)+"\" >"+this.getLinkForResetEmail(user)+"</a>"
-                                + "<br /><br />"
-                                + "If you haven't registed to MeteoCal or if you haven't required to change your password, ignore this eMail.<br />"
-                                + "Best regards,<br />"
-                                + "        MeteoCal's Team"        
-                );            } catch (MessagingException | UnsupportedEncodingException ex) {
+                this.mailControl.sendMail(email,KindOfEmail.FORGOTTENPASSWORD,null );
+            } catch (MessagingException | UnsupportedEncodingException ex) {
                 Logger.getLogger(RecoverPasswordBean.class.getName()).log(Level.SEVERE, null, ex);
             }
             MessageBean.addInfo("Recover link sent to the specified email");
@@ -117,7 +109,6 @@ public class RecoverPasswordBean {
         if(this.email==null){
             Map<String, String> params =FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
             String emailFromEmail = params.get("email");
-            System.out.println(params.keySet().toString() +"-->"+emailFromEmail+"<--");
             if(emailFromEmail!=null){
                 this.email=emailFromEmail;
             }
@@ -178,15 +169,7 @@ public class RecoverPasswordBean {
         this.code = code;
     }
     
-    /**
-     * 
-     * @param user
-     * @return the absolute path to go to the set new password page with correct parameters
-     */
-    private String getLinkForResetEmail(User user) {
-        return "http://localhost:8080/MeteoCal/setNewPassword.xhtml?faces-redirect=true&code="
-                + this.getCodeFromUser(user) + "&email="+user.getEmail();
-    }
+    
     
     /**
      * 
