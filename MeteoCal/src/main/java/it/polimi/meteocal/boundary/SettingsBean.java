2c3c641c739cefda26c3490baf9500172c63d2e1
Fabiuz <Fabiuz@mbp-di-fabiuz.home>
2015-01-11 21:06:31 +0000

    Add the export function

--- MeteoCal/src/main/java/it/polimi/meteocal/boundary/SettingsBean.java
+++ MeteoCal/src/main/java/it/polimi/meteocal/boundary/SettingsBean.java
@@ -1,16 +1,29 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template exportedFile, choose Tools | Templates
  * and open the template in the editor.
  */
 package it.polimi.meteocal.boundary;
 
 import it.polimi.meteocal.control.Utility;
+import it.polimi.meteocal.entity.Event;
 import it.polimi.meteocal.entity.User;
 import it.polimi.meteocal.entityManager.UserManager;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+import org.primefaces.model.DefaultStreamedContent;
 
 /**
  *
@@ -27,9 +40,66 @@ public class SettingsBean {
     
     private String oldPassword;
     private String newPassword;
+    private DefaultStreamedContent exportedFile;
     
     public SettingsBean() {
     }
+    
+    /**
+     * This function create the exportedFile that have to exported
+ NB:il percorso in cui posso scrivere parte da netbeans/glassfish-4.1/
+      fuori dal quella cartella non ho i permessi
+     * @return an empty string
+     */
+    public String export(){
+        User currentUser=userManager.getLoggedUser();
+        try {
+            //Read the user data
+            try (FileWriter out = new FileWriter(currentUser.getEmail()+"_export.xml")) {
+                //Read the user data
+                out.write("<user>\n");
+
+                out.write("\t<email>"+currentUser.getEmail()+"</email>\n");
+                out.write("\t<name>"+currentUser.getName()+"</name>\n");
+                out.write("\t<surname>"+currentUser.getSurname()+"</surname>\n");
+                out.write("\t<birthday>"+currentUser.getBirthDate()+"</birthday>\n");
+                out.write("\t<city>"+currentUser.getCity()+"</city>\n");
+                out.write("\t<address>"+currentUser.getAddress()+"</address>\n");
+                out.write("\t<gender>"+currentUser.getGender()+"</gender>\n");
+                out.write("\t<privatecalendar>"+currentUser.getPrivateCalendar()+"</privatecalendar>\n");
+
+                //for to insert events data into xml exportedFile
+                Set<Event> listOfEventInCalendar=currentUser.getEvents();
+                out.write("\t<events>\n");
+                for(Event event:listOfEventInCalendar){
+                    out.write("\t\t<event>\n");
+                    out.write("\t\t\t<title>"+event.getTitle()+"</title>\n");
+                    out.write("\t\t\t<description>"+event.getDescription()+"</description>\n");
+                    out.write("\t\t\t<locationinfo>"+event.getLocationInfo()+"</locationinfo>\n");
+                    out.write("\t\t\t<city>"+event.getCity()+"</city>\n");
+                    out.write("\t\t\t<address>"+event.getAddress()+"</address>\n");
+                    out.write("\t\t\t<creator>"+event.getCreatedEvent()+"</creator>\n");
+                    out.write("\t\t\t<createdevent>"+event.getCreatedEvent()+"</createdevent>\n");
+                    out.write("\t\t\t<begindate>"+event.getBeginDate()+"</begindate>\n");
+                    out.write("\t\t\t<enddate>"+event.getEndDate()+"</enddate>\n");
+                    out.write("\t\t\t<publicevent>"+event.getPublicEvent()+"</publicevent>\n");
+                    out.write("\t\t\t<indoor>"+event.getIndoor()+"</indoor>\n");
+                    out.write("\t\t</event>\n");
+                }
+                out.write("\t</events>\n");
+                out.write("</user>");
+                out.flush();
+                out.close();
+                InputStream stream = new FileInputStream(currentUser.getEmail()+"_export.xml");
+                this.exportedFile=new DefaultStreamedContent(stream,"text/xml", currentUser.getEmail()+"_export.xml");
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(SettingsBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        MessageBean.addInfo("ExportMessages","Data  correctly exported");
+        return "";
+    }
+
 
     public String changePassword(){
         //verify the old password is correct
@@ -109,6 +179,21 @@ public class SettingsBean {
      */
     public void setUser(User user) {
         this.user = user;
+    }
+
+    /**
+     * @return the exportedFile
+     */
+    public DefaultStreamedContent getFile() {
+        this.export();
+        return exportedFile;
+    }
+
+    /**
+     * @param file the exportedFile to set
+     */
+    public void setFile(DefaultStreamedContent file) {
+        this.exportedFile = file;
     }
 
 }
